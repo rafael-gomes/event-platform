58d0c163bf0a48d199f0e8464904f8c1cfeeb0b9
Rafael Gomes <rafaell.gomes@gmail.com>
2022-06-22 23:58:13 -0300

    Finished day 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { Header } from "./components/Header"
-import { Lesson } from "./components/Lesson"
-import { Sidebar } from "./components/Sidebar"
-import { Video } from "./components/Video"
-import { Event } from "./pages/Event"
+import { ApolloProvider } from "@apollo/client"
+import { BrowserRouter } from "react-router-dom"
+import { client } from "./lib/apollo"
+import { Router } from "./Router"
 
 function App() {
   return (
-    <Event />
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
